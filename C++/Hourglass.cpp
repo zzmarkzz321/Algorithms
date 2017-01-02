6dbf0c50c4bbf99841dd80fd61d2ded9b1ba8c4c
Mark renzo Santiago <ms103804@gmail.com>
2017-01-02 01:57:12 -0800

    add new c++

---
+++ C++/Hourglass.cpp
@@ -0,0 +1,39 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    // create the 2-D Matrix
+    vector< vector<int> > arr(6,vector<int>(6));
+    for(int arr_i = 0;arr_i < 6;arr_i++){
+       for(int arr_j = 0;arr_j < 6;arr_j++){
+          cin >> arr[arr_i][arr_j];
+       }
+    }
+    
+    // Define the hourglass
+    1 1 1 
+    0 1 0
+    1 0 1
+    return 0;
+}
